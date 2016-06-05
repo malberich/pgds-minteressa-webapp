fd0d2ffabccce40fc3dc807476f013d24595ea37
Mario Alberich <malberich@gmail.com>
2016-06-05 16:47:50 +0200

    Server application baseline configuration

---
+++ src/server.js
@@ -0,0 +1,44 @@
+var express    = require('express'),
+	app        = express(),
+	router     = express.Router({mergeParams: true}),
+	mongoose   = require('mongoose'),
+	UserModel  = require('./app/models/user'),
+	passport   = require('./app/modules/passport'),
+	bodyParser = require('body-parser'),
+	port = process.env.port || 8080;
+
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')('replacemesoon'));
+app.use(
+	require('express-session')({
+		secret: 'keyboard cat',
+		resave: false,
+		saveUninitialized: false
+	})
+);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', express.static('webui'));
+
+app.use('/api/users', require('./app/modules/users'));
+app.use('/api/sessions', require('./app/modules/sessions'));
+
+app.use('/api/topics', require('./app/modules/topics'));
+app.use('/api/topics/:topicId/tweets', require('./app/modules/tweets'));
+app.use('/api/topics/:topicId/documents', require('./app/modules/documents'));
+
+app.use('/auth', require('./app/modules/auth'));
+
+mongoose.connect(
+	'mongodb://' +
+	(process.env.mongohost || 'localhost') + ':' +
+	(process.env.mongoport || '27017') + '/' +
+	(process.env.mongodb   || 'minteressa')
+); // connect to our database
+
+app.listen(port);
+
